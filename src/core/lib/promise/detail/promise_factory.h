bd293ace556ccc1d3dc05a008c1224f7ee09f11d
czy67890 <2089808398@qq.com>
2024-01-23 11:02:01 +0800

    comp promise_factory

--- src/core/lib/promise/detail/promise_factory.h
+++ src/core/lib/promise/detail/promise_factory.h
@@ -5,8 +5,8 @@
 #ifndef CZYSERVER_PROMISE_FACTORY_H
 #define CZYSERVER_PROMISE_FACTORY_H
 #include <type_traits>
-
-
+#include <utility>
+#include "core/lib/promise/detail/promise_like.h"
 namespace crpc_core{
 
 
@@ -22,32 +22,55 @@ namespace promise_detail{
     };
 
     template<typename T,typename Ignored = void>
-    struct ResultOfT{
+    struct ResultOfT;
 
+    template <typename F,typename ...Args>
+    struct ResultOfT<F(Args...),std::void_t<decltype( std::declval<std::remove_cvref_t<F>>()(std::declval<Args>()...))>>{
+        using T = decltype( std::declval<std::remove_cvref_t<F>>()(std::declval<Args>()...));
     };
 
     template <typename F,typename ...Args>
-    struct ResultOfT<F(Args...),std::void_t<decltype( std::declval<std::remove_cvref<F>>()(std::declval<Args>()...))>>{
-        using T = decltype( std::declval<std::remove_cvref<F>>()(std::declval<Args>()...));
+    struct ResultOfT<F(Args...)&,std::void_t<decltype( std::declval<std::remove_cvref_t<F>>()(std::declval<Args>()...))>>{
+        using T = decltype( std::declval<std::remove_cvref_t<F>>()(std::declval<Args>()...));
     };
 
     template <typename F,typename ...Args>
-    struct ResultOfT<F(Args...)&,std::void_t<decltype( std::declval<std::remove_cvref<F>>()(std::declval<Args>()...))>>{
-        using T = decltype( std::declval<std::remove_cvref<F>>()(std::declval<Args>()...));
-    };
-
-    template <typename F,typename ...Args>
-    struct ResultOfT<const F(Args...)&,std::void_t<decltype( std::declval<std::remove_cvref<F>>()(std::declval<Args>()...))>>{
-        using T = decltype( std::declval<std::remove_cvref<F>>()(std::declval<Args>()...));
+    struct ResultOfT<const F(Args...)&,std::void_t<decltype( std::declval<std::remove_cvref_t<F>>()(std::declval<Args>()...))>>{
+        using T = decltype( std::declval<std::remove_cvref_t<F>>()(std::declval<Args>()...));
     };
 
     template<typename F>
     using ResultOf = ResultOfT<F>::T;
 
+    /// remember when use it
+    /// we need to remove_cv_ref
+    template<typename F,typename Arg>
+    class Curried{
+    public:
+        Curried(F && f,Arg && arg)
+            :f_(std::forward<F>(f)),arg_(std::forward<Arg>(arg))
+        {}
+
+        using Result = decltype(std::declval<F>()(std::declval<Arg>()));
+
+        Result operator()(){
+            return f_(std::move(arg_));
+        }
+
+    private:
+        [[no_unique_address]]F f_;
+        [[no_unique_address]] Arg arg_;
+    };
+
+    template<typename A,typename F>
+    std::enable_if_t<!IsVoidCallable<ResultOf<F(A)>>::value,PromiseLike<Curried<std::remove_cvref_t<F>,A>>>
+    PromiseFactoryImpl(F && f,A &&arg){
+        return Curried<std::remove_cvref_t<F>,A>(std::forward<F>(f),std::forward<A>(arg));
+    }
+
+    template<typename >
 
 }
-
-
 }
 
 #endif //CZYSERVER_PROMISE_FACTORY_H
--- src/core/lib/promise/detail/promise_like.h
+++ src/core/lib/promise/detail/promise_like.h
@@ -4,7 +4,58 @@
 
 #ifndef CZYSERVER_PROMISE_LIKE_H
 #define CZYSERVER_PROMISE_LIKE_H
+#include <type_traits>
+#include <utility>
+#include "core/lib/promise/poll.h"
 
+namespace crpc_core{
+    namespace promise_detail{
+        template<typename T>
+        struct PollWrapper{
+            static Poll<T> Wrap(T && x){
+                return Poll<T>(std::forward<T>(x));
+            }
+        };
+
+        ///because of has no Poll's Poll
+        ///we partial specialization it
+        template<typename T>
+        struct PollWrapper<Poll<T>>{
+            static Poll<T> Wrap(Poll<T>&& x){
+                return Poll<T> (std::forward<Poll<T>>(x));
+            }
+        };
+
+        template <typename T>
+        auto WrapInPoll(T && x){
+            return PollWrapper<T>::Wrap(x);
+        }
+
+        template <typename F,typename SfinaeVoid = void>
+        class PromiseLike;
+
+        template<>
+        class PromiseLike<void>;
+
+        /// remember this is a partial specialization
+        template<typename F>
+        class PromiseLike<F,std::enable_if_t<!std::is_void_v<std::result_of_t<F()> > >>{
+            PromiseLike(F && f)
+                :f_(std::forward<F>(f)){}
+
+            auto operator()(){
+                return WrapInPoll(f_());
+            }
+
+            using Result = typename PollTraits<decltype(WrapInPoll(F()))>::Type;
+
+        private:
+            [[no_unique_address]] F f_;
+        };
+
+    }
+
+}
 
 
 #endif //CZYSERVER_PROMISE_LIKE_H
